ef4ff2951902b0d0cbcb939652be61846ace0234
Murat Can Koç <muratcankoc@gmail.com>
2024-12-20 10:58:37 +0100

    Initial Commit

---
+++ chatTest.20241220/chatTest_20241220App.swift
@@ -0,0 +1,17 @@
+//
+//  chatTest_20241220App.swift
+//  chatTest.20241220
+//
+//  Created by muratcankoc on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct chatTest_20241220App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
